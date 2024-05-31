74323e24b033d5d02e3ad39973fc2fdeae21e581
Mystic <215104920@qq.com>
2024-05-31 10:02:43 +0800

    feat: parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filepath);
+
+    let contents = std::fs::read_to_string(&config.filepath)
+        .expect("Something went wrong reading the file");
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+        Config { query, filepath }
+    }
+}
+
+struct Config {
+    query: String,
+    filepath: String,
 }
