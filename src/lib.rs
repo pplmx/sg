2f265f83968d9958383bd9994eaaab04e8201899
Mystic <215104920@qq.com>
2024-05-31 15:18:19 +0800

    refactor: simplify the search func

--- src/lib.rs
+++ src/lib.rs
@@ -21,34 +21,22 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = std::fs::read_to_string(config.filepath)?;
-
-    let search = if config.is_case_sensitive {
-        search_sensitive
-    } else {
-        search_insensitive
-    };
-
-    for line in search(&config.query, &contents) {
-        println!("{}", line);
+    let contents = std::fs::read_to_string(&config.filepath)?;
+    let results = search(&config, &contents);
+    for line in results {
+        println!("{line}");
     }
-
     Ok(())
 }
 
-pub fn search_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    contents
-        .lines()
-        .filter(|line| line.to_lowercase().contains(&query))
-        .collect()
+pub fn search<'a>(cfg: &'a Config, contents: &'a str) -> impl Iterator<Item = &'a str> + 'a {
+    contents.lines().filter(move |line| {
+        if cfg.is_case_sensitive {
+            line.contains(&cfg.query)
+        } else {
+            line.to_lowercase().contains(&cfg.query.to_lowercase())
+        }
+    })
 }
 
 #[cfg(test)]
@@ -76,54 +64,68 @@ mod tests {
 
     #[test]
     fn search_one_result() {
-        let query = "safe";
+        let cfg = Config {
+            query: String::from("safe"),
+            filepath: String::from(""),
+            is_case_sensitive: true,
+        };
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
-
-        assert_eq!(
-            vec!["safe, fast, productive."],
-            search_sensitive(query, contents)
-        );
+        let results: Vec<_> = search(&cfg, contents).collect();
+        assert_eq!(vec!["safe, fast, productive."], results);
     }
 
     #[test]
     fn search_multiple_results() {
-        let query = "the";
+        let cfg = Config {
+            query: String::from("the"),
+            filepath: String::from(""),
+            is_case_sensitive: true,
+        };
         let contents = "\
 The a
 The b
 the c
 the d";
-
-        assert_eq!(vec!["the c", "the d"], search_sensitive(query, contents));
+        let results: Vec<_> = search(&cfg, contents).collect();
+        assert_eq!(vec!["the c", "the d"], results);
     }
 
     #[test]
     fn search_no_results() {
-        let query = "fox";
+        let cfg = Config {
+            query: String::from("begin"),
+            filepath: String::from(""),
+            is_case_sensitive: true,
+        };
         let contents = "The end.";
-
-        assert_eq!(Vec::<&str>::new(), search_sensitive(query, contents));
+        let results: Vec<_> = search(&cfg, contents).collect();
+        assert_eq!(Vec::<&str>::new(), results);
     }
 
     #[test]
     fn case_sensitive() {
-        let query = "the";
+        let cfg = Config {
+            query: String::from("the"),
+            filepath: String::from(""),
+            is_case_sensitive: true,
+        };
         let contents = "The a\nThe b\nthe c\nthe d";
-
-        assert_eq!(vec!["the c", "the d"], search_sensitive(query, contents));
+        let results: Vec<_> = search(&cfg, contents).collect();
+        assert_eq!(vec!["the c", "the d"], results);
     }
 
     #[test]
     fn case_insensitive() {
-        let query = "the";
+        let cfg = Config {
+            query: String::from("the"),
+            filepath: String::from(""),
+            is_case_sensitive: false,
+        };
         let contents = "The a\nThe b\nthe c\nthe d";
-
-        assert_eq!(
-            vec!["The a", "The b", "the c", "the d"],
-            search_insensitive(query, contents)
-        );
+        let results: Vec<_> = search(&cfg, contents).collect();
+        assert_eq!(vec!["The a", "The b", "the c", "the d"], results);
     }
 }
