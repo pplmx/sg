6f24283a3cfe010cebfcb9933c63f0e3214d0386
Mystic <215104920@qq.com>
2024-05-31 13:11:15 +0800

    feat: introduce CASE_INSENSITIVE env

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::env;
 use std::error::Error;
 
 pub struct Config {
     pub query: String,
     pub filepath: String,
+    pub is_case_sensitive: bool,
 }
 
 impl Config {
@@ -12,14 +14,44 @@ impl Config {
         }
         let query = args[1].clone();
         let filepath = args[2].clone();
-        Ok(Config { query, filepath })
+
+        // is_err() returns true if the environment variable is not set,
+        // which means the search should be case-sensitive.
+        let is_case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filepath, is_case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = std::fs::read_to_string(config.filepath)?;
-    println!("With text:\n{}", contents);
+
+    let search = if config.is_case_sensitive {
+        search
+    } else {
+        search_case_insensitive
+    };
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -43,5 +75,52 @@ mod tests {
         let args = vec![String::from("sg")];
         let config = Config::from(&args);
         assert!(config.is_err());
+    }
+
+    #[test]
+    fn search_one_result() {
+        let query = "safe";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn search_multiple_results() {
+        let query = "the";
+        let contents = "\
+The a
+The b
+the c
+the d";
+
+        assert_eq!(vec!["the c", "the d"], search(query, contents));
+    }
+
+    #[test]
+    fn search_no_results() {
+        let query = "fox";
+        let contents = "The end.";
+
+        assert_eq!(Vec::<&str>::new(), search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "the";
+        let contents = "The a\nThe b\nthe c\nthe d";
+
+        assert_eq!(vec!["the c", "the d"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "the";
+        let contents = "The a\nThe b\nthe c\nthe d";
+
+        assert_eq!(vec!["The a", "The b", "the c", "the d"], search_case_insensitive(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::from(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filepath);
-
     if let Err(e) = sg::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
